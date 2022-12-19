6092fd66979f15f7d3e6e69696a517ad3c263c28
gioanbatan <giovannibassani@hotmail.com>
2022-12-19 16:07:43 +0100

    Add: first migration

---
+++ database/migrations/2022_12_19_141317_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 20);
+            $table->string('stazione_partenza', 20);
+            $table->string('stazione_arrivo', 20);
+            $table->dateTime('ora_partenza');
+            $table->dateTime('ora_arrivo');
+            $table->string('codice_treno');
+            $table->tinyInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
