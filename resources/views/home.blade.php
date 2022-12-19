1319483d438705bae7c003994a76a7819df718f6
gioanbatan <giovannibassani@hotmail.com>
2022-12-19 17:27:21 +0100

    Add: some graphic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,1 @@
-@extends('title', 'Trains')
+
--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -1,3 +1,10 @@
 @extends ('layouts.app')
 
 @section('title', 'Trains')
+
+@section('content')
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit facilis maxime consectetur? Autem, quod accusamus
+        odio maiores cupiditate tempora velit animi. Reiciendis perspiciatis sequi corporis autem tempora suscipit, maiores
+        tempore?</p>
+
+@endsection
