f92a9c54367f1762e96cfa5bc81cca90aae03bc3
gioanbatan <giovannibassani@hotmail.com>
2022-12-20 17:23:19 +0100

    Add: Cards in train\index

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -4,13 +4,22 @@
 
 @section('content')
     <div class="container">
-        <div class="row row-cols-4">
+        <div class="row row-cols-4 g-3">
             @foreach ($trains as $train)
-                {{ $train->azienda }}
-                {{ $train->stazione_partenza }}
-                {{ $train->ora_partenza }}
-                {{ $train->ora_arrivo }}
-                {{ $train->strazione_arrivo }}
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            Stazione di partenza: {{ $train->stazione_partenza }}
+                            <br>
+                            Stazione di arrivo: {{ $train->strazione_arrivo }}
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Compagnia: {{ $train->azienda }}</li>
+                            <li class="list-group-item">Ora partenza: {{ $train->ora_partenza }}</li>
+                            <li class="list-group-item">Ora arrivo: {{ $train->ora_arrivo }}</li>
+                        </ul>
+                    </div>
+                </div>
             @endforeach
         </div>
     </div>
