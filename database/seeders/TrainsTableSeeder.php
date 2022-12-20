d16f15c1c0fd1d31a96a65a978ac4be68a31993e
gioanbatan <giovannibassani@hotmail.com>
2022-12-20 16:12:02 +0100

    Add: cicle for add 10 entries

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -16,20 +16,22 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $newTrain = new Train();
+        for ($i = 0; $i < 10; $i++) {
+            $newTrain = new Train();
 
-        $newTrain->azienda = $faker->word();
-        $newTrain->stazione_partenza = $faker->text(15);
-        $newTrain->stazione_arrivo = $faker->text(15);
-        $newTrain->ora_partenza = $faker->dateTimeBetween('-1 week', '+2 week');
-        $newTrain->ora_arrivo = $faker->dateTimeInInterval($newTrain->ora_partenza, '+20 hour');
-        $newTrain->codice_treno = $faker->bothify('??###??');
-        $newTrain->numero_carrozze = $faker->numberBetween(1, 20);
-        $newTrain->in_orario = $faker->boolean();
-        $newTrain->cancellato = $faker->boolean();
-        $newTrain->prezzo = $faker->randomFloat(2, 0, 1000);
+            $newTrain->azienda = $faker->word();
+            $newTrain->stazione_partenza = $faker->text(15);
+            $newTrain->stazione_arrivo = $faker->text(15);
+            $newTrain->ora_partenza = $faker->dateTimeBetween('-1 week', '+2 week');
+            $newTrain->ora_arrivo = $faker->dateTimeInInterval($newTrain->ora_partenza, '+20 hour');
+            $newTrain->codice_treno = $faker->bothify('??###??');
+            $newTrain->numero_carrozze = $faker->numberBetween(1, 20);
+            $newTrain->in_orario = $faker->boolean();
+            $newTrain->cancellato = $faker->boolean();
+            $newTrain->prezzo = $faker->randomFloat(2, 0, 1000);
 
-        // dd($newTrain);
-        $newTrain->save();
+            // dd($newTrain);
+            $newTrain->save();
+        }
     }
 }
